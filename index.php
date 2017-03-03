84c6946e4f9ce88c2798174aab2fe13bb93f0b79
gitlet <11thdim@gmail.com>
2017-03-03 05:52:26 +0530

    First commit to the somephp novice to pro proj

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+      echo "Hi";
+      ## single-line comment
+      // also single-line comment
+      /*
+      multi-line
+      comment
+      */
+      $var = 'variable';
+      echo "my first variable name is " . $var;
+      ?>
+  </body>
+</html>
